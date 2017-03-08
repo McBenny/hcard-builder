541853fda55a07371aab29fd91a320af3eb77bfc
McBenny <benoit.adam@yacare.fr>
2017-03-08 23:20:27 +1100

    Assessment v1.0

--- app/app.js
+++ app/app.js
@@ -2,13 +2,10 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+    'ngRoute',
+    'myApp.view1'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
+    $locationProvider.hashPrefix('!');
+    $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,33 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.filter('tel', function() {
+	return function (input) {
+		if (input && input.length > 1) {
+			var re1 = /([0-9]{2})/,
+				re2 = /([0-9]{2})([0-9]{4})/;
+			if (input.length > 2) {
+				if (input.length > 6) {
+					return input.replace(re2, "$1 $2 ");
+				}
+				return input.replace(re1, "$1 ");
+			}
+		}
+		return input;
+	};
+})
 
+.controller('View1Ctrl', ['$scope', 'telFilter', function($scope, telFilter) {
+	document.getElementById("upload").onchange = function () {
+	    var reader = new FileReader();
+
+	    reader.onload = function (e) {
+	        document.getElementById("avatar").src = e.target.result;
+	    };
+
+	    // read the image file as a data URL.
+	    reader.readAsDataURL(this.files[0]);
+	};
 }]);
+
+
