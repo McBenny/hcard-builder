7a620e465539aefc6b9a5966cedf6ab2bc1fc2bd
McBenny <benoit.adam@yacare.fr>
2017-03-09 00:04:08 +1100

    Mobile adjustments and read me update

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -10,32 +10,41 @@ angular.module('myApp.view1', ['ngRoute'])
 }])
 
 .filter('tel', function() {
-	return function (input) {
-		if (input && input.length > 1) {
-			var re1 = /([0-9]{2})/,
-				re2 = /([0-9]{2})([0-9]{4})/;
-			if (input.length > 2) {
-				if (input.length > 6) {
-					return input.replace(re2, "$1 $2 ");
-				}
-				return input.replace(re1, "$1 ");
-			}
-		}
-		return input;
-	};
+    return function (input) {
+        if (input && input.length > 1) {
+            var re1 = /([0-9]{2})/,
+                re2 = /([0-9]{2})([0-9]{4})/;
+            if (input.length > 2) {
+                if (input.length > 6) {
+                    return input.replace(re2, "$1 $2 ");
+                }
+                return input.replace(re1, "$1 ");
+            }
+        }
+        return input;
+    };
 })
 
 .controller('View1Ctrl', ['$scope', 'telFilter', function($scope, telFilter) {
-	document.getElementById("upload").onchange = function () {
-	    var reader = new FileReader();
+    document.getElementById("upload").onchange = function () {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            document.getElementById("avatar").src = e.target.result;
+        };
+        reader.readAsDataURL(this.files[0]);
+    };
 
-	    reader.onload = function (e) {
-	        document.getElementById("avatar").src = e.target.result;
-	    };
-
-	    // read the image file as a data URL.
-	    reader.readAsDataURL(this.files[0]);
-	};
+    var sizesEgalizer = function () {
+        var hcard = document.getElementById('hcard'),
+            hcardPreview = document.getElementById('hcard-preview'),
+            hcardH = hcard.offsetHeight,
+            hcardPreviewH = hcardPreview.offsetHeight;
+console.log(document.body.clientWidth);
+        if (document.body.clientWidth >= 992 && hcardH > hcardPreviewH) {
+            hcardPreview.style.height = hcardH + 'px';
+        }
+    };
+    sizesEgalizer();
 }]);
 
 
